632d02af67c01536a3eee4cd7b567c6f4e0abc2a
Jack Noordhuis <daswaggamer@gmail.com>
2019-02-16 02:16:21 +1100

    Hack for not double closing global namespaces

--- src/BasicCompiler.php
+++ src/BasicCompiler.php
@@ -18,8 +18,16 @@ declare(strict_types=1);
 
 namespace jacknoordhuis\phpcompiler;
 
+use function count;
 use jacknoordhuis\phpcompiler\exception\CompilerException;
+use const PHP_EOL;
+use PhpParser\Lexer;
+use PhpParser\Node;
 use PhpParser\Node\Stmt\Namespace_;
+use function preg_replace;
+use function str_replace;
+use const T_DOLLAR_OPEN_CURLY_BRACES;
+use function var_dump;
 
 /**
  * The basic compiler which performs next to no changes to to the code.
@@ -45,52 +53,68 @@ class BasicCompiler extends Compiler {
 
 		$parsed = $this->parser->parse($content);
 		$stmts = $this->traverser->traverseFile($parsed, $file);
-		$pretty = $this->printer->prettyPrint($stmts);
 
-		$pretty = preg_replace(
-			'#^(<\?php)?[\s]*(/\*\*?.*?\*/)?[\s]*(declare[\s]*\([\s]*strict_types[\s]*=[\s]*1[\s]*\);)?#s',
-			'',
-			$pretty
-		);
-
-		return $this->getCodeWrappedIntoNamespace($parsed, $pretty);
+		return $this->getCodeWrappedIntoNamespace($stmts);
 	}
 
 	/**
 	 * Wrap the code into a namespace.
 	 *
 	 * @param array  $parsed
-	 * @param string $pretty
 	 *
 	 * @return string
 	 */
-	protected function getCodeWrappedIntoNamespace(array $parsed, string $pretty) : string {
-		if($this->parsedCodeHasNamespaces($parsed)) {
-			$pretty = preg_replace('/^\s*(namespace.*);/im', '${1} {', $pretty, 1) . "\n}\n";
+	protected function getCodeWrappedIntoNamespace(array $parsed) : string {
+		if(count($nodes = $this->getParsedCodeNamespaces($parsed)) !== 0) { //has a namespace node
+			$pretty = "";
+			foreach($nodes as $namespace) {
+				if($pretty !== "") {
+					$pretty .= PHP_EOL;
+				}
+
+				if($namespace->name === null) { //hack for
+					$namespace->name = new Node\Name("global");
+				}
+
+				$pretty .= preg_replace('/^\s*(namespace.*);/im', '${1} {', $this->getPrettyPrinted([$namespace]), 1) . "\n}\n";
+			}
 		} else {
-			$pretty = sprintf("namespace {\n%s\n}\n", $pretty);
+			$pretty = sprintf("namespace {\n%s\n}\n", $this->getPrettyPrinted($parsed));
 		}
 
 		return preg_replace('/(?<!.)[\r\n]+/', '', $pretty);
 	}
 	/**
-	 * Check parsed code for having namespaces.
+	 * Check parsed code for having namespaces and return them.
 	 *
 	 * @param array $parsed
 	 *
-	 * @return bool
+	 * @return array
 	 */
-	protected function parsedCodeHasNamespaces(array $parsed) : bool {
+	protected function getParsedCodeNamespaces(array $parsed) : array {
 		// Namespaces can only be on first level in the code,
 		// so we only make one check on it.
-		$node = array_filter(
+		return array_filter(
 			$parsed,
 			function($value) {
 				return $value instanceof Namespace_;
 			}
 		);
+	}
 
-		return !empty($node);
+	/**
+	 * Get pretty printed code with strict type declaration stripped.
+	 *
+	 * @param array $nodes
+	 *
+	 * @return string|string[]|null
+	 */
+	protected function getPrettyPrinted(array $nodes) {
+		return preg_replace(
+			'#^(<\?php)?[\s]*(\/\*\*?.*?\*\/)?[\s]*(declare[\s]*\([\s]*strict_types[\s]*=[\s]*1[\s]*\);)?#s',
+			'',
+			preg_replace('/^(namespace) global(;.*)\Z/msA', "$1$2", $this->printer->prettyPrint($nodes))
+		);
 	}
 
 }
